f2492bcc6e627cf650b12bcae92a89f3b2dfe832
shivapbhusal <civabhusal@gmail.com>
2017-09-16 05:58:36 -0400

    variables added

---
+++ variables.py
@@ -0,0 +1,9 @@
+width=17
+height=12 
+delimiter='.'
+print(width/2) #division
+print (float(width)/2) # casting : changing one variable type to another 
+print(width/2.0)
+print(height/3)
+print(1+2*5)
+print(delimiter*5)
